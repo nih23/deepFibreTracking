db6153edcd2ca016e2732030c39f7ce7f806b6c9
Jos Torge <jos.torge@icloud.com>
2020-05-06 17:15:01 +0200

    Finished first CSDBasedTracker, not tested yet

--- src/tracker.py
+++ src/tracker.py
@@ -1,3 +1,4 @@
+"""Implementing different tracker classes"""
 from dipy.tracking.utils import random_seeds_from_mask, seeds_from_mask
 from dipy.tracking.local_tracking import LocalTracking
 from dipy.tracking.stopping_criterion import ThresholdStoppingCriterion
@@ -9,15 +10,50 @@ from dipy.data import get_sphere
 from dipy.direction import peaks_from_model
 import dipy.reconst.dti as dti
 
+from src.data import Object
 from src.config import Config
 
 
+class Error(Exception):
+    """Base class for Tracker exceptions."""
+
+    def __init__(self, msg=''):
+        self.message = msg
+        Exception.__init__(self, msg)
+
+    def __repr__(self):
+        return self.message
+
+    __str__ = __repr__
+
+class StreamlinesAlreadyTrackedError(Error):
+    """Error thrown if streamlines are already tracked."""
+
+    def __init__(self, tracker):
+        self.tracker = tracker
+        self.data_container = tracker.data_container
+        Error.__init__(self, msg=("There are already {sl} streamlines tracked out of dataset {id}. "
+                                  .format(sl=len(tracker.streamlines), id=self.data_container.id),
+                                  "Create a new Tracker object to change parameters."))
+class StreamlinesNotTrackedError(Error):
+    """Error thrown if streamlines weren't tracked yet."""
+
+    def __init__(self, tracker):
+        self.tracker = tracker
+        self.data_container = tracker.data_container
+        Error.__init__(self, msg=("The streamlines weren't tracked yet from Dataset {id}. "
+                                  .format(id=self.data_container.id),
+                                  "Call Tracker.track() to track the streamlines."))
 class Tracker():
+    """Universal Tracker class"""
     def __init__(self, data_container):
-        pass
+        self.data_container = data_container
+    def track(self):
+        """Track given data"""
 
 
 class SeedBasedTracker(Tracker):
+    """Seed based tracker"""
     def __init__(self, data_container,
                  random_seeds=None,
                  seeds_count=None,
@@ -25,62 +61,73 @@ class SeedBasedTracker(Tracker):
                  step_size=None,
                  min_length=None,
                  max_length=None):
+        Tracker.__init__(self, data_container)
+        self.options = Object()
         if random_seeds is None:
-            random_seeds = Config.get_config().getboolean("tracking", "useRandomSeeds", fallback="no")
+            random_seeds = Config.get_config().getboolean("tracking", "randomSeeds", fallback="no")
         if seeds_count is None:
             seeds_count = Config.get_config().getint("tracking", "seedsCount", fallback="30000")
         if seeds_per_voxel is None:
-            seeds_per_voxel = Config.get_config().getboolean("tracking", "seedsPerVoxel", fallback="no")
+            seeds_per_voxel = Config.get_config().getboolean("tracking", "seedsPerVoxel",
+                                                             fallback="no")
         if step_size is None:
             step_size = Config.get_config().getfloat("tracking", "stepSize", fallback="1.0")
         if min_length is None:
-            min_length = Config.get_config().getfloat("tracking", "minimumStreamlineLength", fallback="20")
+            min_length = Config.get_config().getfloat("tracking", "minimumStreamlineLength",
+                                                      fallback="20")
         if max_length is None:
-            max_length = Config.get_config().getfloat("tracking", "maximumStreamlineLength", fallback="200")
+            max_length = Config.get_config().getfloat("tracking", "maximumStreamlineLength",
+                                                      fallback="200")
         self.streamlines = None
-        (_, _, gtab, _, aff, _, _, binarymask) = data_container.data
-        if not random_seeds:
-            seeds = seeds_from_mask(binarymask, affine=aff)
-        else:
-            seeds = random_seeds_from_mask(binarymask, seeds_count=seeds_count, 
-                                           seed_count_per_voxel=seeds_per_voxel, affine=aff)
-        self.seeds = seeds
-        self.mask = binarymask
-        self.gtab = gtab
-        self.max_length = max_length
-        self.min_length = min_length
-        self.step_size = step_size
-        self.aff = aff
         self.data = data_container.data
-    def track(self, classifier, directionGetter):
+        if not random_seeds:
+            seeds = seeds_from_mask(self.data.binarymask, affine=self.data.aff)
+        else:
+            seeds = random_seeds_from_mask(self.data.binarymask, seeds_count=seeds_count,
+                                           seed_count_per_voxel=seeds_per_voxel,
+                                           affine=self.data.aff)
+        self.seeds = seeds
+        self.options.max_length = max_length
+        self.options.min_length = min_length
+        self.options.step_size = step_size
+    def _track(self, classifier, direction_getter):
         if self.streamlines is not None:
-            pass # TODO Error
-        streamlines_generator = LocalTracking(directionGetter, classifier, self.seeds, self.aff,
-                                              step_size=self.step_size)
+            raise StreamlinesAlreadyTrackedError(self) from None
+        streamlines_generator = LocalTracking(direction_getter, classifier, self.seeds,
+                                              self.data.aff, step_size=self.options.step_size)
         streamlines = Streamlines(streamlines_generator)
-        streamlines = self.filterStreamlinesByLength(streamlines, minimumLength=self.min_length, 
-                                                     maximumLength=self.max_length)
+        streamlines = self.filter_streamlines_by_length(streamlines,
+                                                        minimum=self.options.min_length,
+                                                        maximum=self.options.max_length)
         self.streamlines = streamlines
 
     def get_streamlines(self):
+        """Retrieve the calculated streamlines"""
         if self.streamlines is None:
-            pass # TODO Error
+            raise StreamlinesNotTrackedError(self) from None
         return self.streamlines
 
-    def saveToFile(self, path):
+    def save_to_file(self, path):
+        """Save the calculated streamlines to file"""
         if self.streamlines is None:
-            pass # TODO Error
+            raise StreamlinesNotTrackedError(self) from None
         save_vtk_streamlines(self.streamlines, path)
 
-    def filterStreamlinesByLength(self, streamlines, 
-                                  minimumLength=Config.get_config().getfloat("tracking", "minimumStreamlineLength",fallback="20"),
-                                  maximumLength=Config.get_config().getfloat("tracking", "maximumStreamlineLength", fallback="200")):
+    def filter_streamlines_by_length(self, streamlines,
+                                     minimum=Config.get_config()
+                                     .getfloat("tracking", "minimumStreamlineLength",
+                                               fallback="20"),
+                                     maximum=Config.get_config()
+                                     .getfloat("tracking", "maximumStreamlineLength",
+                                               fallback="200")):
         """
         removes streamlines that are shorter than minimumLength (in mm)
         """
-        return [x for x in streamlines if metrics.length(x) > minimumLength and metrics.length(x) < maximumLength]
+        return [x for x in streamlines if metrics.length(x) > minimum
+                and metrics.length(x) < maximum]
 
 class CSDTracker(SeedBasedTracker):
+    """A CSD based Tracker"""
     def __init__(self, data_container,
                  random_seeds=None,
                  seeds_count=None,
@@ -89,21 +136,23 @@ class CSDTracker(SeedBasedTracker):
                  min_length=None,
                  max_length=None,
                  fa_threshold=None):
-        SeedBasedTracker.__init__(self, data_container, random_seeds, seeds_count, seeds_per_voxel, step_size, min_length, max_length)
+        SeedBasedTracker.__init__(self, data_container, random_seeds, seeds_count, seeds_per_voxel,
+                                  step_size, min_length, max_length)
         if fa_threshold is None:
             fa_threshold = Config.get_config().getfloat("tracking", "faTreshhold", fallback="0.15")
-        self.fa_threshold = fa_threshold
+        self.options.fa_threshold = fa_threshold
 
     def track(self):
-        (_, _, gtab, dwi, _, _, _, binarymask) = self.data
-        response, _ = auto_response(gtab, dwi, roi_radius=10, fa_thr=0.7)
-        csd_model = ConstrainedSphericalDeconvModel(gtab, response)
-        directionGetter = peaks_from_model(model=csd_model,
-                                     data=dwi,
-                                     sphere=get_sphere('symmetric724'),
-                                     mask=binarymask,
-                                     relative_peak_threshold=.5,
-                                     min_separation_angle=25,
-                                     parallel=False)
-        dti_fit = dti.TensorModel(gtab, fit_method='LS').fit(dwi, mask=binarymask)
-        SeedBasedTracker.track(self, ThresholdStoppingCriterion(dti_fit.fa, self.fa_threshold), directionGetter)
+        response, _ = auto_response(self.data.gtab, self.data.dwi, roi_radius=10, fa_thr=0.7)
+        csd_model = ConstrainedSphericalDeconvModel(self.data.gtab, response)
+        direction_getter = peaks_from_model(model=csd_model,
+                                            data=self.data.dwi,
+                                            sphere=get_sphere('symmetric724'),
+                                            mask=self.data.binarymask,
+                                            relative_peak_threshold=.5,
+                                            min_separation_angle=25,
+                                            parallel=False)
+        dti_fit = dti.TensorModel(self.data.gtab, fit_method='LS')
+        dti_fit = dti_fit.fit(self.data.dwi, mask=self.data.binarymask)
+        self._track(ThresholdStoppingCriterion(dti_fit.fa, self.options.fa_threshold),
+                    direction_getter)
