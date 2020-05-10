d60bd30717ffaf647809c63c7ffe213f402f30e5
Jos Torge <jos.torge@icloud.com>
2020-05-10 16:19:58 +0200

    Started working on dataset. Not runnable currently

---
+++ src/dataset.py
@@ -0,0 +1,76 @@
+from src.data import MovableData
+import torch
+
+class BaseDataset(MovableData):
+    def __init__(self, data_container, device=None):
+        MovableData.__init__(self, device=device)
+        self.data_container = data_container
+        self.id = str(self.__class__.__name__)
+        if data_container is not None:
+            self.id = self.id + "[" + str(data_container.id) + "]"
+    
+
+class IterableDataset(BaseDataset, torch.utils.data.Dataset):
+    def __init__(self, data_container, device=None):
+        BaseDataset.__init__(self, data_container, device=device)
+        torch.utils.data.Dataset.__init__(self)
+
+    def __len__(self):
+        if type(self) is IterableDataset:
+            raise NotImplementedError() from None
+
+    def __getitem__(self, index):
+        if type(self) is IterableDataset:
+            raise NotImplementedError() from None
+
+
+class ConcatenatedDataset(IterableDataset):
+    """A class usable to concatenate multiple datasets.
+    Same type is not necessary, but recommended for practical use.
+    """
+    def __init__(self, datasets, device=None):
+        IterableDataset.__init__(self, None, device=device)
+        self.id = self.id + "["
+        self.__lens = [0]
+        for dataset in datasets:
+            if not isinstance(dataset, MovableData) or not isinstance(dataset, IterableDataset):
+                raise WrongDatasetTypePassedError(self) from None # TODO
+            dataset.to(self.device)
+            self.id = self.id + dataset.id + ", "
+            self.__lens.append(len(dataset) + self.__lens[-1])
+        self.id = self.id[:-2] + "]"
+        self.datasets = datasets
+
+    def __len__(self):
+        return self.__lens[-1]
+    def __getitem__(self, index):
+        if index > len(self):
+            raise IndexError('index {i} out of bounds for ConcatenatedDataset with length {l}.'
+                             .format(i=index, l=len(self))) from None
+        i = 0
+        while self.__lens[i+1] <= index:
+            i = i + 1
+
+        return self.datasets[i][index - self.__lens[i]]
+
+    def cuda(self, device=None, non_blocking=False, memory_format=torch.preserve_format):
+        """Moves all Tensors to specified CUDA device"""
+        for dataset in self.datasets:
+            dataset.cuda(device=device, non_blocking=non_blocking,
+                         memory_format=memory_format)
+            self.device = dataset.device
+        return self
+
+    def cpu(self, memory_format=torch.preserve_format):
+        """Moves all Tensors to CPU"""
+        for dataset in self.datasets:
+            dataset.cpu(memory_format=memory_format)
+            self.device = dataset.device
+        return self
+
+    def to(self, *args, **kwargs):
+        """Moves all tensors to specified device"""
+        for dataset in self.datasets:
+            dataset.to(*args, **kwargs)
+            self.device = dataset.device
+        return self
