a4db1e510b3bf224a1abbd92ee31107a6b16676b
Jos Torge <jos.torge@icloud.com>
2020-05-13 23:46:10 +0200

    Bugfix

--- src/dataset.py
+++ src/dataset.py
@@ -164,6 +164,6 @@ class StreamlineDataset(IterableDataset):
             rotation_arr = np.ones([len(direction_array), 3, 3])
             for i in range(len(direction_array)):
                 rotation_from_vectors(rotation_arr[i, :, :], reference, direction_array[i, :])
-                direction_array[i, :] = rotation_arr[i, :, :].T @ direction_array[i, :] #just for testing, should return reference vectors 
+                direction_array[i, :] = rotation_arr[i].T @ direction_array[i] #just for testing, should return reference vectors 
         direction_array = np.concatenate((direction_array, np.array([[0, 0, 0]])))
         return direction_array
