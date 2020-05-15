350dd95654c1499bbf39a06bd89fb59a5b5c84eb
Jos Torge <jos.torge@icloud.com>
2020-05-15 22:54:41 +0200

    Providing simple usage example

---
+++ code_example.py
@@ -0,0 +1,18 @@
+"""Just example code as explanation. Usable for testing."""
+from src.data import HCPDataContainer
+from src.dataset import StreamlineDataset, ConcatenatedDataset
+from src.tracker import CSDTracker, DTITracker
+
+def main():
+    """Main method"""
+    data = HCPDataContainer(100307)
+    csd_sl = CSDTracker(data, random_seeds=True, seeds_count=10000)
+    dti_sl = DTITracker(data, random_seeds=True, seeds_count=10000)
+    csd_sl.track()
+    dti_sl.track()
+    csd = StreamlineDataset(csd_sl, data)
+    dti = StreamlineDataset(dti_sl, data)
+    dataset = ConcatenatedDataset([csd, dti])
+
+if __name__ == "__main__":
+    main()
