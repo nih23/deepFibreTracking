0ee4fe1d199446a0285ece423a279e957131b394
Jos Torge <jos.torge@icloud.com>
2020-05-20 15:06:39 +0200

    Updated code_example

--- code_example.py
+++ code_example.py
@@ -1,5 +1,6 @@
 """Just example code as explanation. Usable for testing."""
 from src.data import HCPDataContainer
+from src.data.postprocessing import res100
 from src.dataset import StreamlineDataset, ConcatenatedDataset
 from src.tracker import CSDTracker, DTITracker
 
@@ -11,8 +12,8 @@ def main():
     csd_sl.track()
     dti_sl.track()
     data = data.crop().normalize()
-    csd = StreamlineDataset(csd_sl, data)
-    dti = StreamlineDataset(dti_sl, data)
+    csd = StreamlineDataset(csd_sl, data, postprocessing=res100())
+    dti = StreamlineDataset(dti_sl, data, postprocessing=res100())
     dataset = ConcatenatedDataset([csd, dti])
 
 if __name__ == "__main__":
