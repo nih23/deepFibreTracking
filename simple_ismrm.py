55a8878535f4724a45c5eb75fac0269c8b36203a
Jos Torge <jos.torge@icloud.com>
2020-06-07 18:44:25 +0200

    First bugfixes

--- simple_ismrm.py
+++ simple_ismrm.py
@@ -1,10 +1,12 @@
-from src.tracker import SeedBasedTracker
+from src.tracker import SeedBasedTracker, StreamlinesAlreadyTrackedError
+from src.config import Config
+from src.cache import Cache
 from src.util import get_reference_orientation, rotation_from_vectors, get_grid
 import numpy as np
 class ModelTracker(SeedBasedTracker):
     """A CSD based Tracker"""
     def __init__(self, data_container,
-                 model
+                 model,
                  random_seeds=None,
                  seeds_count=None,
                  seeds_per_voxel=None,
@@ -22,7 +24,7 @@ class ModelTracker(SeedBasedTracker):
     def _track(self):
         if self.streamlines is not None:
             raise StreamlinesAlreadyTrackedError(self) from None
-        
+
         streamlines = self._trackWithModel()
         streamlines = self.filter_streamlines_by_length(streamlines,
                                                         minimum=self.options.min_length,
@@ -31,20 +33,23 @@ class ModelTracker(SeedBasedTracker):
         model = self.model.cuda()
         data_container = self.data_container
         seeds = self.seeds
-        grid = get_grid((7,3,3)))
+        grid = get_grid((7,3,3))
         streamlines = np.zeros((2* len(seeds), no_iterations + 1, 3))
         streamlines[:len(seeds), 0] = seeds
         streamlines[len(seeds):,0] = seeds
         for idx in range(0, len(seeds), batch_size):
             model.reset()
-            idx_range = range(idx, min(idx+batchSize, len(seeds)))    
-            idx_range_back = range(idx + len(seeds), min(idx+batchSize, len(seeds)) + len(seeds))
+            idx_range = range(idx, min(idx+batch_size, len(seeds)))    
+            idx_range_back = range(idx + len(seeds), min(idx+batch_size, len(seeds)) + len(seeds))
 
             #First prediction
             points = streamlines[idx_range, 0]
             points_w_grid = points[:, None, None, None, :] + grid
-            dwi = data_container.get_interpolated_dwi(points_w_grid).reshape(len(points), grid.size, -1)
+            dwi = data_container.get_interpolated_dwi(points_w_grid)
             print(dwi.shape)
+            dwi = dwi.reshape(len(points), grid.size, -1)
+            print(dwi.shape)
+        return 0
 
     def track(self):
         SeedBasedTracker.track(self)
