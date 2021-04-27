ab78cce93312303bfec2e94e67dba9b6126c6920
Pia Hanfeld <pia.hanfeld@gmail.com>
2021-04-27 13:23:37 +0200

    Add cosine similarity reward to environment

--- dfibert/envs/RLtractEnvironment.py
+++ dfibert/envs/RLtractEnvironment.py
@@ -58,65 +58,109 @@ class RLtractEnvironment(gym.Env):
         return interpolated_dwi
   
     
-    def step(self, action):
-        #self.reward -= 0.1
-        self.stepCounter += 1
-        rewardNextState = 0
 
+    def step(self, action):
+        self.stepCounter += 1
+        if self.stepCounter >= self.maxSteps:
+            distTerminal = self.rewardForTerminalState(self.state)
+            if distTerminal < 2.5:
+                return self.state, 1., True#, {}
+            else:
+                return self.state, -distTerminal, True#, {}
+        done = False
         if action == (self.action_space.n - 1):
-            #reward = self.rewardForState(self.state)
-            #self.stepCounter += 1
-            #return self.state, reward, False
-            #print(action)
+            distTerminal = self.rewardForTerminalState(self.state)
+            if distTerminal < 2.5:
+                print("Defi stopped at/close to the terminal state!")
+                return self.state, 1., True#, {}
             nextState = self.state
-            dist_to_terminal = self.rewardForTerminalState(nextState)
-            if dist_to_terminal < 0.1:
-                print("Hey, hey, hey we finally stopped at the terminal state! :D")
-                return nextState, 1, True
         else:
-            ## convert discrete action into tangent vector
             action_vector = self.directions[action]
-            
-            ## apply step by step length and update state accordingly
             positionNextState = self.state.getCoordinate() + self.stepWidth * action_vector
+            #positionNextState = self.state.getCoordinate() + action    # <- for continous action space
             nextState = TractographyState(positionNextState, self.interpolateDWIatState)
             if nextState.getValue() is None:
                 rewardNextState = self.rewardForTerminalState(nextState)
-                return self.state, -rewardNextState, True
+                return  self.state, -rewardNextState, True#, {}
         
-        ## compute reward for new state
-
-
-        ### check if we already left brain map
-        # => RLenv.dataset.data.binarymask.shape
-        # set done = True if coordinate of nextState is outside of binarymask
-        done = False
-        # try:
-        #     nextState.getValue()
-        # except PointOutsideOfDWIError:
-        #     print("PointOutside still occured")
-        #     done = True
-        #     #print("Agent left brain mask :(")
-        #     return self.state, -100, done
-
-        rewardNextState = self.rewardForState(nextState)
-        if rewardNextState < -np.exp(2*(0.81 )-1):
-          done = True
-        # if rewardNextState < 0.:
-        #     done = True
-        if self.stepCounter > self.maxSteps:
-            #if rewardNextState > 0.:
-            #    rewardNextState = 50    
-            done = True       
         
-        # if self.points_visited == len(self.referenceStreamline_ijk):
-        #     print("Hey, hey, hey we finally visited all tiles! :D")
-        #     done = True
-        #     #rewardNextState = 100
-
+        rewardNextState = self.cosineSimReward(self.state, nextState)
+        
         self.state = nextState
         # return step information
-        return nextState, rewardNextState, done
+        return self.state, rewardNextState, done#, {}
+    
+    
+    def cosineSimReward(self, state, nextState):
+        current_index = np.min([self.stepCounter,len(self.referenceStreamline_ijk)-1])
+        path_vector = (nextState.getCoordinate() - state.getCoordinate()).squeeze(0)
+        reference_vector = self.referenceStreamline_ijk[current_index]-self.referenceStreamline_ijk[current_index-1]
+        cosine_sim = torch.nn.functional.cosine_similarity(path_vector, reference_vector, dim=0)
+        dist = torch.sum((self.referenceStreamline_ijk[current_index] - nextState.getCoordinate())**2)
+        if dist < 2.5:
+            dist = 0
+        else:
+            dist = dist - 2.5
+        return cosine_sim-dist
+    # def step(self, action):
+    #     #self.reward -= 0.1
+    #     self.stepCounter += 1
+    #     rewardNextState = 0
+
+    #     if action == (self.action_space.n - 1):
+    #         #reward = self.rewardForState(self.state)
+    #         #self.stepCounter += 1
+    #         #return self.state, reward, False
+    #         #print(action)
+    #         nextState = self.state
+    #         dist_to_terminal = self.rewardForTerminalState(nextState)
+    #         if dist_to_terminal < 0.1:
+    #             print("Hey, hey, hey we finally stopped at the terminal state! :D")
+    #             return nextState, 1, True
+    #     else:
+    #         ## convert discrete action into tangent vector
+    #         action_vector = self.directions[action]
+            
+    #         ## apply step by step length and update state accordingly
+    #         positionNextState = self.state.getCoordinate() + self.stepWidth * action_vector
+    #         nextState = TractographyState(positionNextState, self.interpolateDWIatState)
+    #         if nextState.getValue() is None:
+    #             rewardNextState = self.rewardForTerminalState(nextState)
+    #             return self.state, -rewardNextState, True
+        
+    #     ## compute reward for new state
+
+
+    #     ### check if we already left brain map
+    #     # => RLenv.dataset.data.binarymask.shape
+    #     # set done = True if coordinate of nextState is outside of binarymask
+    #     done = False
+    #     # try:
+    #     #     nextState.getValue()
+    #     # except PointOutsideOfDWIError:
+    #     #     print("PointOutside still occured")
+    #     #     done = True
+    #     #     #print("Agent left brain mask :(")
+    #     #     return self.state, -100, done
+
+    #     rewardNextState = self.rewardForState(nextState)
+    #     if rewardNextState < -np.exp(2*(0.81 )-1):
+    #       done = True
+    #     # if rewardNextState < 0.:
+    #     #     done = True
+    #     if self.stepCounter > self.maxSteps:
+    #         #if rewardNextState > 0.:
+    #         #    rewardNextState = 50    
+    #         done = True       
+        
+    #     # if self.points_visited == len(self.referenceStreamline_ijk):
+    #     #     print("Hey, hey, hey we finally visited all tiles! :D")
+    #     #     done = True
+    #     #     #rewardNextState = 100
+
+    #     self.state = nextState
+    #     # return step information
+    #     return nextState, rewardNextState, done
     
 
     
