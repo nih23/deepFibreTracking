b570e83d9df534bbd1b9007490399546ef66b99d
phanfeld <pia.hanfeld@gmail.com>
2021-05-14 15:35:11 +0200

    Variable action space
    + streamline index can be chosen on environment reset
    + action space size can be set as parameter while initializing the environment
    + promising tests on 20 actions and low step width

--- dfibert/envs/RLtractEnvironment.py
+++ dfibert/envs/RLtractEnvironment.py
@@ -6,6 +6,7 @@ from gym.spaces import Discrete, Box
 import numpy as np
 
 from dipy.data import get_sphere
+from dipy.data import HemiSphere, Sphere
 import torch
 
 
@@ -19,7 +20,7 @@ from ._state import TractographyState
 
 
 class RLtractEnvironment(gym.Env):
-    def __init__(self, device, stepWidth = 1, dataset = '100307', grid_dim = [3,3,3], maxL2dist_to_terminalState = 0.1, pReferenceStreamlines = "data/HCP307200_DTI_smallSet.vtk"):
+    def __init__(self, device, stepWidth = 1, action_space=20, dataset = '100307', grid_dim = [3,3,3], maxL2dist_to_terminalState = 0.1, pReferenceStreamlines = "data/HCP307200_DTI_smallSet.vtk"):
         #data/HCP307200_DTI_min40.vtk => 5k streamlines
         print("Loading precomputed streamlines (%s) for ID %s" % (pReferenceStreamlines, dataset))
         self.device = device
@@ -28,10 +29,17 @@ class RLtractEnvironment(gym.Env):
         
         self.stepWidth = stepWidth
         self.dtype = torch.FloatTensor
-        sphere = get_sphere("repulsion100")
+        #sphere = HemiSphere(xyz=res)#get_sphere("repulsion100")
+        n_pts = action_space - 1                                    ## added action_space variable for flexible debugging
+        theta = np.pi * np.random.rand(n_pts)
+        phi = 2 * np.pi * np.random.rand(n_pts)
+        sphere = Sphere(theta=theta, phi=phi)
         self.directions = sphere.vertices
+        self.directions = np.concatenate((self.directions, np.array([[0.0, 0.0, 0.0]])))
+        #self.directions = sphere.vertices #res
         noActions, _ = self.directions.shape
-        self.action_space = spaces.Discrete(noActions+1) # spaces.Discrete(noActions)
+        #noActions = len(self.directions)
+        self.action_space = spaces.Discrete(noActions) #spaces.Discrete(noActions+1)
         self.dwi_postprocessor = resample(sphere=sphere)
         self.referenceStreamline_ijk = None
         self.grid = get_grid(np.array(grid_dim))
@@ -63,28 +71,33 @@ class RLtractEnvironment(gym.Env):
         self.stepCounter += 1
         if self.stepCounter >= self.maxSteps:
             distTerminal = self.rewardForTerminalState(self.state)
-            if distTerminal < 2.5:
+            if distTerminal < 0.05:
                 return self.state, 1., True#, {}
             else:
                 return self.state, -distTerminal, True#, {}
         done = False
         if action == (self.action_space.n - 1):
             distTerminal = self.rewardForTerminalState(self.state)
-            if distTerminal < 2.5:
+            if distTerminal < 0.05:
                 print("Defi stopped at/close to the terminal state!")
                 return self.state, 1., True#, {}
             nextState = self.state
-        else:
-            action_vector = self.directions[action]
-            positionNextState = self.state.getCoordinate() + self.stepWidth * action_vector
-            #positionNextState = self.state.getCoordinate() + action    # <- for continous action space
-            nextState = TractographyState(positionNextState, self.interpolateDWIatState)
-            if nextState.getValue() is None:
-                rewardNextState = self.rewardForTerminalState(nextState)
-                return  self.state, -rewardNextState, True#, {}
+        #else:
+        action_vector = self.directions[action]
+        positionNextState = self.state.getCoordinate() + self.stepWidth * action_vector
+        #positionNextState = self.state.getCoordinate() + action_vector
+        #positionNextState = self.state.getCoordinate() + action    # <- for continous action space
+        nextState = TractographyState(positionNextState, self.interpolateDWIatState)
+        if nextState.getValue() is None:
+            rewardNextState = self.rewardForTerminalState(nextState)
+            return  self.state, -rewardNextState, True#, {}
         
         
-        rewardNextState = self.cosineSimReward(self.state, nextState)
+        rewardNextState = - self.rewardForState(nextState)#self.cosineSimReward(self.state, nextState)
+        #distTerminal = self.rewardForTerminalState(self.state)
+        #if distTerminal < 2.5:
+        #    print("Defi stopped at/close to the terminal state!")
+        #    return self.state, 1., True#, {}
         
         self.state = nextState
         # return step information
@@ -96,12 +109,13 @@ class RLtractEnvironment(gym.Env):
         path_vector = (nextState.getCoordinate() - state.getCoordinate()).squeeze(0)
         reference_vector = self.referenceStreamline_ijk[current_index]-self.referenceStreamline_ijk[current_index-1]
         cosine_sim = torch.nn.functional.cosine_similarity(path_vector, reference_vector, dim=0)
-        dist = torch.sum((self.referenceStreamline_ijk[current_index] - nextState.getCoordinate())**2)
-        if dist < 2.5:
-            dist = 0
-        else:
-            dist = dist - 2.5
-        return cosine_sim-dist
+        #dist = torch.sum((self.referenceStreamline_ijk[current_index] - nextState.getCoordinate())**2)
+        #dist = torch.dist()
+        #if dist < 2.5:
+        #    dist = 0
+        #else:
+        #    dist = dist - 2.5
+        return cosine_sim#-dist
     # def step(self, action):
     #     #self.reward -= 0.1
     #     self.stepCounter += 1
@@ -176,6 +190,10 @@ class RLtractEnvironment(gym.Env):
         qry_pt = state.getCoordinate().view(-1,3)
         l2_distance = torch.sum((self.referenceStreamline_ijk[current_index] - qry_pt)**2)
 
+        #if l2_distance < 0.2:
+        #    l2_distance = 0.
+        #else:
+        #    l2_distance = np.abs(0.2-l2_distance)
         #rewardNextState = -torch.exp(2*l2_distance-1)
         #distance = torch.sum((self.referenceStreamline_ijk[current_index] - qry_pt)**2)
         
@@ -209,12 +227,13 @@ class RLtractEnvironment(gym.Env):
         return distance
 
     # reset the game and returns the observed data from the last episode
-    def reset(self):       
+    def reset(self, streamline_index=None):       
         file_sl = StreamlinesFromFileTracker(self.pReferenceStreamlines)
         file_sl.track()
         
         tracked_streamlines = file_sl.get_streamlines()
-        streamline_index = np.random.randint(len(tracked_streamlines))
+        if streamline_index == None:
+            streamline_index = np.random.randint(len(tracked_streamlines))
         #print("Reset to streamline %d/%d" % (streamline_index+1, len(tracked_streamlines)))
         referenceStreamline_ras = tracked_streamlines[streamline_index]
         referenceStreamline_ijk = self.dataset.to_ijk(referenceStreamline_ras)
